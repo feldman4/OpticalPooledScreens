39b76b12851874568704e576b7a30964c930fcb2
David Feldman <dfeldman@uw.edu>
2020-11-30 12:22:50 -0800

    rearrange

--- DF/ops/imports.py
+++ DF/ops/imports.py
@@ -15,6 +15,7 @@ import numpy as np
 import pandas as pd
 import skimage
 
+from .annotate import annotate_labels, annotate_points, annotate_bases
 from .annotate import GRMC
 from .io import BLUE, GREEN, RED, MAGENTA, GRAY, CYAN, GLASBEY 
 from .io import grid_view
@@ -25,13 +26,13 @@ from .filenames import timestamp, file_frame
 from .io import read_stack as read
 from .io import save_stack as save
 
-from .utils import or_join, and_join
-from .utils import groupby_reduce_concat, groupby_histogram, replace_cols
-from .utils import pile, montage, make_tiles, trim, join_stacks, csv_frame
-
-from .annotate import annotate_labels, annotate_points, annotate_bases
+from .utils import (
+    or_join, and_join,
+    groupby_reduce_concat, groupby_histogram, replace_cols,
+    pile, montage, make_tiles, trim, join_stacks,
+    csv_frame,
+)
 
 from .plates import add_global_xy, add_row_col
-
 from .pool_design import reverse_complement as rc
-
+from . import in_situ
