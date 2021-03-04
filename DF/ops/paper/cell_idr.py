3c3328a0295013b34f2afeb8ef25794219d55980
David Feldman <dfeldman@uw.edu>
2021-03-03 21:48:47 -0800

    setup example

--- DF/ops/paper/cell_idr.py
+++ DF/ops/paper/cell_idr.py
@@ -1,3 +1,16 @@
+import fire
+import os
+import sys
+import subprocess
+
+import pandas as pd
+
+package_dir = os.path.sep.join(
+    os.path.normpath(__file__).split(os.path.sep)[:-3])
+aspera_openssh = os.path.join(package_dir, 'resources/asperaweb_id_dsa.openssh')
+ascp_guess = os.path.join(os.environ['HOME'], '.aspera/connect/bin/ascp')
+
+
 def write_pairlist(df_files, filename, remote_prefix='20191104-gcloud/'):
     """Write list of filenames for use with ascp --file-pair-list option.
     """
@@ -8,11 +21,74 @@ def write_pairlist(df_files, filename, remote_prefix='20191104-gcloud/'):
 
     with open(filename, 'w') as fh:
         fh.write(txt)
-        
-def format_ascp_command(ascp, aspera_openssh, pairlist, local='.'):
+
+
+def format_ascp_command(ascp, pairlist, local='.'):
     """Generate Aspera download command. Requires paths to ascp executable and SSH key. 
     See https://idr.openmicroscopy.org/about/download.html
     """
     ascp_opts = f'-T -l200m -P 33001 -i {aspera_openssh}'
     pair_opts = f'--file-pair-list={pairlist} --mode=recv --user=idr0071 --host=fasp.ebi.ac.uk'
     return f'{ascp} {ascp_opts} {pair_opts} {local}'
+
+
+def setup_example(directory, ascp=ascp_guess, well='A1', tile='102'):
+    """Create a fresh analysis directory for Cell IDR experiment C (A549 cells, CROPseq library, 
+    p65 antibody).
+    """
+    try:
+        os.makedirs(directory)
+    except FileExistsError as e:
+        print(f'Error: directory already exists at {os.path.abspath(directory)}')
+        raise QuitError
+
+    # link resources
+    links = {
+        'CROPseq_NFkB_library.csv': 'barcodes.csv',
+        'experimentC_example.yaml': 'config.yaml',
+        'Cell_IDR.smk': 'Snakefile',
+    }
+    for here, there in links.items():
+        os.symlink(f'{package_dir}/resources/{here}', f'{directory}/{there}')
+        print(f'Linked {there}')
+
+    if not os.path.exists(ascp):
+        print(f'Error: Aspera ascp executable not found at {ascp}')
+        raise QuitError
+
+    # select our example
+    select_tile = 'idr_name == "experimentC" & well == @well & tile == @tile'
+    select_image_tags = 'tag == ["phenotype", "sbs"]'    
+    df_idr = (pd.read_csv(f'{package_dir}/resources/Cell_IDR_files.csv.gz', low_memory=False)
+     .query(select_tile)
+     .query(select_image_tags)
+    )
+
+    pairlist = f'{directory}/ascp_download_list.txt'
+    write_pairlist(df_idr, pairlist)
+    command = format_ascp_command(ascp, pairlist, local=directory)
+
+    print(f'Downloading {len(df_idr)} files from Cell-IDR with command: {command}')
+    subprocess.run(command, shell=True)
+
+    well_tile_list = f'{directory}/experimentC/well_tile_list_example.csv'
+    pd.DataFrame({'well': [well], 'tile': [tile]}).to_csv(well_tile_list, index=None)
+
+    print('Setup complete.\nTo run the example snakemake pipeline, execute the following:')
+    print(f'cd {directory}')
+    print('snakemake --cores --configfile=config.yaml')
+
+
+class QuitError(Exception):
+    """Don't generate a stack trace if encountered in command line app.
+    """
+    pass
+
+if __name__ == '__main__':
+    commands = {
+        'setup_example': setup_example
+    }
+    try:
+        fire.Fire(commands)
+    except QuitError:
+        sys.exit(1)
