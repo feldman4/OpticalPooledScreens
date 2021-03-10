c70d9c7d3e3b2c05313806f491c2d086778e4721
lukebfunk <lfunkonline@gmail.com>
2021-03-10 12:14:24 -0500

    Revert "tqdm imports"
    This reverts commit 24fad586f7585019983eafb5d60c35428a7f0917.

--- DF/ops/triangle_hash.py
+++ DF/ops/triangle_hash.py
@@ -215,15 +215,10 @@ def remove_overlap(xs, ys):
     ys = set(map(tuple, ys))
     return [tuple(x) for x in xs if tuple(x) not in ys]
 
-def brute_force_pairs(df_0, df_1, n_jobs=-2, tqdn=True):
-    if tqdn:
-        from tqdm.auto import tqdm
-        work = tqdm(work = df_1.groupby('site'),'site')
-    else:
-        work = df_1.groupby('site')
-
+def brute_force_pairs(df_0, df_1):
+    from tqdm import tqdm_notebook as tqdn
     arr = []
-    for site, df_s in work:
+    for site, df_s in tqdn(df_1.groupby('site'), 'site'):
 
         def work_on(df_t):
             rotation, translation, score = evaluate_match(df_t, df_s)
@@ -248,8 +243,8 @@ def parallel_process(func, args_list, n_jobs, tqdn=True):
     from joblib import Parallel, delayed
     work = args_list
     if tqdn:
-        from tqdm.auto import tqdm
-        work = tqdm(work, 'work')
+        from tqdm import tqdm_notebook 
+        work = tqdm_notebook(work, 'work')
     return Parallel(n_jobs=n_jobs)(delayed(func)(*w) for w in work)
 
 
