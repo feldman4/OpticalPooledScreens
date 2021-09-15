80a49ba0e25ccbced2829175408e5f523327f24b
lukebfunk <lfunkonline@gmail.com>
2021-09-15 14:00:45 -0400

    update setup.py
    specified skimage and scipy versions require python version >=3.7

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='ops-lasagna',
     version='0.1',
+    python_requires='>=3.7',
     description='Analysis code for pooled optical screening.',  # Required
     # long_description=long_description,
-    url='http://github.com/feldman4/NatureProtocols',
+    url='http://github.com/feldman4/OpticalPooledScreens',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
